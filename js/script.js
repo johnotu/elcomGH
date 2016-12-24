3549a1a952f44764125255c0ed3dda27d9629492
johnotu <johnne01@gmail.com>
2016-12-24 18:27:43 +0000

    Added https to DevLess CDN call

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-	var constants = { "token":"1c953b74aff7b08ee80d5e4b2f5a8651", "domain":"http://frosty-shape-9375.herokuapp.com." }; Devless = new Devless(constants);
+	var constants = { "token":"1c953b74aff7b08ee80d5e4b2f5a8651", "domain":"https://frosty-shape-9375.herokuapp.com." }; Devless = new Devless(constants);
 
 	$('#submitname').click(function(){
 		var name = $('#name').val();
